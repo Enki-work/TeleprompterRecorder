83493301a22787157068cafac503bc4f2d822f79
Enki <wdydhd@vip.qq.com>
2021-11-25 11:49:29 +0900

    録画中にフォーマット変更を禁止する

--- TeleprompterRecorder/TeleprompterRecorder/View/CaptureButtonsView.swift
+++ TeleprompterRecorder/TeleprompterRecorder/View/CaptureButtonsView.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class CaptureButtonsView: UIView {
     @IBOutlet weak var recordBtn: UIButton!
     @IBOutlet weak var formatChangeBtn: UIButton!
     @IBOutlet weak var changeCameraBtn: UIButton!
     
+    let disposeBag = DisposeBag()
+    
     override func awakeFromNib() {
-        
+        recordBtn.rx.tap.map({[weak self] in self?.recordBtn.isSelected ?? true}).bind(to: formatChangeBtn.rx.isEnabled).disposed(by: disposeBag)
     }
 }
