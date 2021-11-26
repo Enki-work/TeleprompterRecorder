881815855d89f7dde49f07b8086d93593aecbeea
Enki <wdydhd@vip.qq.com>
2021-11-26 12:31:39 +0900

    ボタンによりプロンプター表示非表示できるように対応する

--- TeleprompterRecorder/TeleprompterRecorder/View/CaptureButtonsView.swift
+++ TeleprompterRecorder/TeleprompterRecorder/View/CaptureButtonsView.swift
@@ -13,13 +13,16 @@ class CaptureButtonsView: UIView {
     @IBOutlet weak var recordBtn: UIButton!
     @IBOutlet weak var formatChangeBtn: UIButton!
     @IBOutlet weak var changeCameraBtn: UIButton!
+    @IBOutlet weak var prompterBtn: UIButton!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textViewBg: UIView!
     
     let disposeBag = DisposeBag()
     
     override func awakeFromNib() {
         formatChangeBtn.imageView?.contentMode = .scaleAspectFit
         changeCameraBtn.imageView?.contentMode = .scaleAspectFit
+        prompterBtn.imageView?.contentMode = .scaleAspectFit
         recordBtn.rx.tap.map({[weak self] in self?.recordBtn.isSelected ?? true}).bind(to: formatChangeBtn.rx.isEnabled).disposed(by: disposeBag)
         recordBtn.rx.tap.map({[weak self] in self?.recordBtn.isSelected ?? true}).bind(to: changeCameraBtn.rx.isEnabled).disposed(by: disposeBag)
         let obserable = NotificationCenter.default.rx.notification(Notification.Name.init(rawValue: "AVSystemController_SystemVolumeDidChangeNotification")).take(until: self.rx.deallocated).map { _ in}
@@ -57,5 +60,12 @@ class CaptureButtonsView: UIView {
             self.formatChangeBtn.isEnabled = true
             self.changeCameraBtn.isEnabled = true
         }.disposed(by: disposeBag)
+        
+        let willPrompterBtnSelect = prompterBtn.rx.tap.asDriver().map({ [weak self] () -> Bool in
+                guard let self = self else {return false}
+            return !self.prompterBtn.isSelected
+        })
+        willPrompterBtnSelect.asObservable().bind(to: prompterBtn.rx.isSelected).disposed(by: disposeBag)
+        willPrompterBtnSelect.asObservable().bind(to: textViewBg.rx.isHidden).disposed(by: disposeBag)
     }
 }
