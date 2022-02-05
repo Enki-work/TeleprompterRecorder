d28876d6fafc4a7b6118239f10398d9668d39fc0
Enki <wdydhd@vip.qq.com>
2022-02-05 12:50:09 +0900

    メールで連絡画面とロジック実装

---
+++ TeleprompterRecorder/TeleprompterRecorder/ViewController/ContactMeViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ContactMeViewController.swift
+//  TeleprompterRecorder
+//
+//  Created by YanQi on 2022/02/03.
+//
+
+import UIKit
+import MessageUI
+
+class ContactMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    @IBAction func mailMeBtnClick(_ sender: Any) {
+        // Modify following variables with your text / recipient
+        let recipientEmail = "daqige2333@email.com"
+        let subject = "プロンプターカメラについて"
+        let body = ""
+        
+        // Show default mail composer
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients([recipientEmail])
+            mail.setSubject(subject)
+            mail.setMessageBody(body, isHTML: false)
+            
+            present(mail, animated: true)
+            
+            // Show third party email composer if default Mail app is not present
+        } else if let emailUrl = createEmailUrl(to: recipientEmail, subject: subject, body: body) {
+            UIApplication.shared.open(emailUrl)
+        }
+    }
+}
+
+extension ContactMeViewController: MFMailComposeViewControllerDelegate {
+    
+    private func createEmailUrl(to: String, subject: String, body: String) -> URL? {
+        let subjectEncoded = subject.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+        let bodyEncoded = body.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+
+        let gmailUrl = URL(string: "googlegmail://co?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        let outlookUrl = URL(string: "ms-outlook://compose?to=\(to)&subject=\(subjectEncoded)")
+        let yahooMail = URL(string: "ymail://mail/compose?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        let sparkUrl = URL(string: "readdle-spark://compose?recipient=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        let defaultUrl = URL(string: "mailto:\(to)?subject=\(subjectEncoded)&body=\(bodyEncoded)")
+
+        if let gmailUrl = gmailUrl, UIApplication.shared.canOpenURL(gmailUrl) {
+            return gmailUrl
+        } else if let outlookUrl = outlookUrl, UIApplication.shared.canOpenURL(outlookUrl) {
+            return outlookUrl
+        } else if let yahooMail = yahooMail, UIApplication.shared.canOpenURL(yahooMail) {
+            return yahooMail
+        } else if let sparkUrl = sparkUrl, UIApplication.shared.canOpenURL(sparkUrl) {
+            return sparkUrl
+        }
+
+        return defaultUrl
+    }
+
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+    }
+}
---
+++ TeleprompterRecorder/TeleprompterRecorder/ViewController/MenuViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MenuViewController.swift
+//  TeleprompterRecorder
+//
+//  Created by YanQi on 2022/02/03.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SideMenu
+
+class MenuViewController: UITableViewController {
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.rx.itemSelected.subscribe(onNext: { [weak self] indexPath in
+            guard let self = self else {
+                return
+            }
+            self.tableView.deselectRow(at: indexPath, animated: true)
+            switch indexPath.section {
+            case 0:
+                switch indexPath.row {
+                case 0:
+                    let vc = (self.presentingViewController as? UINavigationController)?.topViewController
+                    vc?.performSegue(withIdentifier: "showContactMe", sender: nil)
+                    SideMenuManager.default.rightMenuNavigationController?.dismiss(animated: false, completion: nil)
+                default:
+                    break
+                }
+            case 1:
+                switch indexPath.row {
+                case 0:
+                    //何もしない
+                    return
+                default:
+                    break
+                }
+            default:
+                break
+            }
+        }).disposed(by: disposeBag)
+    }
+
+}
